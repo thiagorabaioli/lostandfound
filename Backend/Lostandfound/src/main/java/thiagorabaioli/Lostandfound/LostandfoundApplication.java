8a22493961a980d1500c35f49a5fc23f2cc1fea4
thiagorabaioli <tfrabaioli@outlook.com>
2025-04-17 15:48:05 +0100

    version 1

--- Backend/Lostandfound/src/main/java/thiagorabaioli/Lostandfound/LostandfoundApplication.java
+++ Backend/Lostandfound/src/main/java/thiagorabaioli/Lostandfound/LostandfoundApplication.java
@@ -8,6 +8,7 @@ public class LostandfoundApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LostandfoundApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
