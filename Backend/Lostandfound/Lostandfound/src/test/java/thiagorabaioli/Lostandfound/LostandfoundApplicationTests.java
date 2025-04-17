d08bc6a378d852814184a4ee7f5aef5fbafda827
thiagorabaioli <tfrbaioli@outlook.com>
2025-04-17 15:13:03 +0100

    first commit

---
+++ Backend/Lostandfound/Lostandfound/src/test/java/thiagorabaioli/Lostandfound/LostandfoundApplicationTests.java
@@ -0,0 +1,13 @@
+package thiagorabaioli.Lostandfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostandfoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
