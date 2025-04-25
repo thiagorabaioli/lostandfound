9e1fb65511de5fb97ebab3cae2e602776150b98c
thiagorabaioli <tfrabaioli@outlook.com>
2025-04-25 10:58:10 +0100

    equals and hashcode

--- Backend/Lostandfound/src/main/java/thiagorabaioli/Lostandfound/entities/ItemLostUserAPPPK.java
+++ Backend/Lostandfound/src/main/java/thiagorabaioli/Lostandfound/entities/ItemLostUserAPPPK.java
@@ -32,4 +32,37 @@ public class ItemLostUserAPPPK {
     public void setUserAPP(UserAPP userAPP) {
         this.userAPP = userAPP;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((itemLost == null) ? 0 : itemLost.hashCode());
+        result = prime * result + ((userAPP == null) ? 0 : userAPP.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ItemLostUserAPPPK other = (ItemLostUserAPPPK) obj;
+        if (itemLost == null) {
+            if (other.itemLost != null)
+                return false;
+        } else if (!itemLost.equals(other.itemLost))
+            return false;
+        if (userAPP == null) {
+            if (other.userAPP != null)
+                return false;
+        } else if (!userAPP.equals(other.userAPP))
+            return false;
+        return true;
+    }
+
+    
 }
